360ab6d9714822b03f3b636d10a0d67a1458a85a
yumzen <yeom.ji.eun35@gmail.com>
2023-07-31 03:34:12 +0900

    #3 Feat: Navbar/Footer 디자인 수정

--- homepage/src/app/components/Navbar.tsx
+++ homepage/src/app/components/Navbar.tsx
@@ -36,69 +36,69 @@ let Links =[
 ];
 
 return (
-    <nav className="w-full shadow-sm bg-white fixed z-10 py-[10px] px-3 border-b items-center justify-center">
-    <div className="px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-10 gap-5">
-        {/* LOGO */}
-        <div className="flex items-center justify-between">
-            <Link href="/">
-                <Image src="/Logo.png" alt="Logo" width={200} height={40} style={{margin : "12px", maxWidth:"236px", maxHeight:"32px"}} />
-            </Link>
+    <nav className="w-full shadow-sm bg-white fixed z-10 py-[20px] px-[20px] border-b">
+        <div className="mx-auto lg:max-w-7xl md:items-center md:flex gap-[16px]">
+            {/* LOGO */}
+            <div className="flex items-center justify-between">
+                <Link href="/">
+                    <Image src="/Logo.png" alt="Logo" width={192} height={36} style={{maxWidth:"192px", maxHeight:"36px"}} />
+                </Link>
             
-            <div className="md:hidden">
-                <button className="p-2 outline-none" onClick={toggleMenu}>
-                {showMenu ? (
-                    <Image src="/close.svg" width={30} height={30} alt="logo" />
-                ) : (
-                    <Image
-                    src="/hamburger.svg"
-                    width={30}
-                    height={30}
-                    alt="logo"
-                    className="focus:border-none active:border-none"
-                    />
-                )}
-                </button>
+                <div className="md:hidden">
+                    <button className="p-2 outline-none" onClick={toggleMenu}>
+                    {showMenu ? (
+                        <Image src="/close.svg" width={32} height={32} alt="logo" />
+                    ) : (
+                        <Image
+                        src="/hamburger.svg"
+                        width={32}
+                        height={32}
+                        alt="logo"
+                        className="focus:border-none active:border-none"
+                        />
+                    )}
+                    </button>
+                </div>
+            </div>
+
+            <div
+            className={`${
+                showMenu ? "mt-8 mb-8 flex flex-col" : "hidden flex-row justify-between px-8 md:px-0"
+            } md:flex w-full items-center`}
+            >
+                <ul className={`${ showMenu ? "flex-col py-[40px] mt-12 mb-4": "flex-col md:flex md:flex-row md:gap-[15px]"}`}>
+                    {
+                        Links.map((link) => (
+                        <li key={link.name} className={` text-[#000] text-center hover:font-semibold ${ showMenu ? "text-[20px] w-[100%] h-[100%] mb-4":"text-[13px] w-[85px] h-[32px] font-medium flex-shrink-0 flex justify-center items-center"}`}>
+                            <Link href={link.link} onClick={closeMenu} >{link.name}</Link>
+                        </li>
+                        ))
+                    }
+                </ul>
+
+                <ul className={`${
+                    showMenu ? " my-12 flex flex-row gap-[60px] items-center justify-center mx-12":"hidden lg:flex lg:flex-row flex-row gap-[12px] items-center justify-center"}`}>
+                    <li className="w-[100%] h-[100%] mx-2 ">
+                    <Link href="https://www.youtube.com/@kahluaband8409" passHref>
+                        <Image src="/youtube.png" alt="유튜브" width={24} height={18} />
+                    </Link>
+                    </li>
+                    <li className="w-[100%] h-[100%] mx-2">
+                    <Link href="https://www.youtube.com/@kahluaband8409" passHref>
+                        <Image src="/kakaotalk.png" alt="카카오톡 채널" width={22} height={22} />
+                    </Link>
+                    </li>
+                    <li className="w-[100%] h-[100%] mx-2">
+                    <Link
+                        href="https://instagram.com/kahlua_band_?igshid=MzRlODBiNWFlZA=="
+                        passHref
+                    >
+                        <Image src="/instagram.png" alt="인스타그램" width={20} height={20} />
+                    </Link>
+                    </li>
+                </ul>
             </div>
         </div>
-
-        <div
-        className={`${
-            showMenu ? "mt-8 mb-8 flex flex-col" : "hidden flex-row justify-between px-8 md:px-0"
-        } md:flex w-full items-center`}
-        >
-        <ul className={`${ showMenu ? "flex-col py-[40px] mt-12 mb-4": "flex-col md:flex md:flex-row md:gap-7"}`}>
-            {
-                Links.map((link) => (
-                <li key={link.name} className={` text-black py-2 text-center hover:font-semibold ${ showMenu ? "text-[20px] w-[100%] h-[100%] mb-4":"text-[14px] w-[92px] h-[32px]"}`}>
-                    <Link href={link.link} onClick={closeMenu} >{link.name}</Link>
-                </li>
-                ))
-            }
-        </ul>
-
-        <ul className={`${
-            showMenu ? " my-12 flex flex-row gap-[60px] items-center justify-center mx-12":"hidden lg:flex lg:flex-row flex-row gap-[12px] items-center justify-center"}`}>
-            <li className="w-[100%] h-[100%] mx-2 ">
-            <Link href="https://www.youtube.com/@kahluaband8409" passHref>
-                <Image src="/youtube.png" alt="유튜브" width={24} height={18} />
-            </Link>
-            </li>
-            <li className="w-[100%] h-[100%] mx-2">
-            <Link href="https://www.youtube.com/@kahluaband8409" passHref>
-                <Image src="/kakaotalk.png" alt="카카오톡 채널" width={22} height={22} />
-            </Link>
-            </li>
-            <li className="w-[100%] h-[100%] mx-2">
-            <Link
-                href="https://instagram.com/kahlua_band_?igshid=MzRlODBiNWFlZA=="
-                passHref
-            >
-                <Image src="/instagram.png" alt="인스타그램" width={20} height={20} />
-            </Link>
-            </li>
-        </ul>
-        </div>
-    </div>
     </nav>
 );
 }
