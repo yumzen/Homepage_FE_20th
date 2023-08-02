f9b44542f0c9e10b614f311fb751aacb5d4fde73
yumzen <yeom.ji.eun35@gmail.com>
2023-08-03 00:52:56 +0900

    #8 Fixes: pages routing Related to: #6

--- homepage/src/app/page.tsx
+++ homepage/src/app/page.tsx
@@ -1,9 +1,8 @@
-import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="font-['pretendard']">
-      Main page
+    <div>
+      Main page!
     </div>
   );
 }
