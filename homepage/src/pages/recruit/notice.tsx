1a588f930d26436d6638bb5fba5da38bda1f5981
limgahyun <limgh2002@gmail.com>
2023-08-06 04:53:59 +0900

    FAQ
    FAQ 완성

--- homepage/src/pages/recruit/notice.tsx
+++ homepage/src/pages/recruit/notice.tsx
@@ -5,25 +5,25 @@ export default function Notice(){
     return(
         <div className="w-full py-16 flex flex-col justify-center items-center">
             <Image src={recruit_icon} alt="notice"/>
-            <p className="text-xl mt-4 mb-8">모집정보</p>
+            <p className="text-xl mt-4 mb-8 font-bold">모집정보</p>
 
-            <p className="text-base mt-4">지원자격</p>
+            <p className="text-base font-semibold mt-4">지원자격</p>
             <p className="text-sm mt-1">
                 홍익대학교 컴퓨터공학과 or 자율전공학부에서 컴퓨터공학과 진입 예정인 24학번 신입생
             </p>
 
-            <p className="text-base mt-4">모집 세션</p>
+            <p className="text-base font-semibold mt-4">모집 세션</p>
             <p className="text-sm mt-1">
                 보컬 2명 / 기타 4명 / 드럼 2명 / 베이스 2명 / 신디사이저 2명
             </p>
 
-            <p className="text-base mt-4">모집 일정</p>
+            <p className="text-base font-semibold mt-4">모집 일정</p>
             <p className="text-center text-sm mt-1">
                 지원서 마감 : ~ 2024.03.08 (금) 23:59까지<br/>
                 보컬부문 지원 영상 : ~ 2024.03.10 (일) 23:59까지
             </p>
 
-            <p className="text-base mt-4">오디션 일정</p>
+            <p className="text-base font-semibold mt-4">오디션 일정</p>
             <p className="text-center text-sm mt-1">
                 2024.03.11(월) 4시-7시<br/>
                 오디션 이후 뒷풀이 예정
