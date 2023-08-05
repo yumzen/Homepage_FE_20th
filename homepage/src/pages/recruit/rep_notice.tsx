b443e4869dbb3875bf8a1335f6a309179294ee94
limgahyun <limgh2002@gmail.com>
2023-08-06 00:55:39 +0900

    tailwind ocean palette
    palette 생성

--- homepage/src/pages/recruit/rep_notice.tsx
+++ homepage/src/pages/recruit/rep_notice.tsx
@@ -1,6 +1,6 @@
 export default function RepNotice() {
     return (
-        <div className="w-full h-784 bg-#0047FF mt-64">
+        <div className="w-full h-784 bg-ocean-700 mt-64">
             KAHLUA
         </div>
     )
--- homepage/tailwind.config.js
+++ homepage/tailwind.config.js
@@ -13,6 +13,23 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      "ocean": {
+        50: '#e8f7ff',
+        100: '#d5f0ff',
+        200: '#b3e1ff',
+        300: '#85caff',
+        400: '#56a5ff',
+        500: '#2f7fff',
+        600: '#0c52ff',
+        700: '#0047ff',
+        800: '#0640cd',
+        900: '#103e9f',
+        950: '#0a225c',
+      },
+    }
   },
   plugins: [],
+
 }
+
