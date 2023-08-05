89b092cfdb5789b381e6b7bee6484ec2bea2b7f8
limgahyun <limgh2002@gmail.com>
2023-08-06 03:37:28 +0900

    notice
    notice.tsx 완료

--- homepage/src/pages/recruit/index.tsx
+++ homepage/src/pages/recruit/index.tsx
@@ -1,9 +1,11 @@
+import Notice from "./notice";
 import RepNotice from "./rep_notice";
 
 export default function Recruit() {
     return (
         <div className="font-pretendard py-8">
             <RepNotice/>
+            <Notice/>
         </div>
     )
 }
--- homepage/src/pages/recruit/rep_notice.tsx
+++ homepage/src/pages/recruit/rep_notice.tsx
@@ -3,26 +3,23 @@ import recruit_rocket from "/public/assets/icons/recruit_rocket.png";
 
 export default function RepNotice() {
     return (
-        <div className="w-full h-auto bg-ocean-700 bg-opacity-10 py-16">
-            <div className="text-center font-Salvar text-9xl">
+        <div className="w-full h-auto bg-ocean-700 bg-opacity-10 py-16 flex flex-col justify-center items-center">
+            <p className="font-Salvar text-9xl">
                 KAHLUA
-            </div>
-            <div className="text-center text-5xl font-bold mt-4">
+            </p>
+            <p className="text-center text-5xl font-bold mt-4">
                 23nd MEMBER<br/>RECUIRMENT
-            </div>
-            <div className="text-center text-xs font-bold mt-2">
+            </p>
+            <p className="text-center text-xs font-bold mt-2">
                 모집기간 : 2023.03.01(금) ~ 2023.03.07(토)
                 <br/>
                 면접기간 : 2023.03.10(월) ~ 2023.03.11(화)
-            </div>
-            <div className="flex justify-center mt-6">
-                <button className="flex justify-center space-x-4 items-center h-[96px] w-5/6 rounded-3xl bg-ocean-700 ">
-                    <Image src={recruit_rocket} alt=".."/>
-                    <div className="text-5xl font-semibold">KAHLUA 23기 지원하기</div>
-                    <Image src={recruit_rocket} alt=".."/>
-                </button>
-
-            </div>
+            </p>
+            <button className="flex justify-center space-x-4 items-center h-[96px] w-5/6 rounded-3xl bg-ocean-700 mt-6">
+                <Image src={recruit_rocket} alt=".."/>
+                <p className="text-5xl font-semibold">KAHLUA 23기 지원하기</p>
+                <Image src={recruit_rocket} alt=".."/>
+            </button>
         </div>
     )
 }
