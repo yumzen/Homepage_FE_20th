775f83ffa04d23e04e8f2df1a19af9745ec39557
caminobelllo <alstpgml0812@gmail.com>
2023-08-06 14:57:12 +0900

    Design: 이미지 슬라이더 전체 수정

--- homepage/src/pages/about/ImageSlider2.tsx
+++ homepage/src/pages/about/ImageSlider2.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "react-slideshow-image/dist/styles.css";
-import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/20/solid";
-import { Zoom, Slide } from "react-slideshow-image";
-import styles from "./ImageSlider.module.css";
+import { useState } from "react";
 import Image from "next/image";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Transition } from "@headlessui/react";
 
 export default function ImageSlider2() {
+  const [active, setActive] = useState<number>(0);
   const images = [
     "/assets/images/about/ensemble2.jpg",
     "/assets/images/about/ensemble3.jpg",
@@ -16,42 +16,98 @@ export default function ImageSlider2() {
     "/assets/images/about/ensemble8.jpg",
   ];
 
-  const zoomInProperties = {
-    scale: 1,
-    duration: 5000,
-    transitionDuration: 300,
-    infinity: true,
+  function prevIndexHandler() {
+    if (active === 0) {
+      setActive(images.length - 1);
+    } else {
+      setActive(active - 1);
+    }
+  }
 
-    // icons
-    prevArrow: (
-      <div className="flex ml-10 justify-center items-center content-center text-center">
-        <ArrowLeftIcon className="h-8 w-8 text-white cursor-pointer" />
-      </div>
-    ),
-    nextArrow: (
-      <div className="flex mr-10 justify-center items-center content-center text-center ">
-        <ArrowRightIcon className="h-8 w-8 text-white cursor-pointer" />
-      </div>
-    ),
-  };
+  function nextIndexHandler() {
+    if (active === images.length - 1) {
+      setActive(0);
+    } else {
+      setActive(active + 1);
+    }
+  }
 
   return (
-    <div className="w-full h-[332px]]">
-      <Zoom {...zoomInProperties}>
-        {images.map((each, index) => (
-          <div
-            key={index}
-            className="flex justify-center item-center relative w-[460px] h-[320px]"
-          >
-            <Image
-              src={each}
-              alt="image-slider"
-              layout="fill"
-              className={styles.image}
-            />
+    <div className="relative w-full h-full">
+      {/* Testimonial image */}
+      <div>
+        <div className="flex justify-center item-center">
+          <div>
+            {images.map((each, index) => (
+              <Transition
+                key={index}
+                show={active === index}
+                className="flex justify-center item-center"
+              >
+                <Image
+                  key={index}
+                  className="relative top-4 left-1/2 -translate-x-1/2 rounded-[30px]"
+                  src={each}
+                  width={480}
+                  height={320}
+                  alt="image slider"
+                />
+              </Transition>
+            ))}
           </div>
-        ))}
-      </Zoom>
+        </div>
+      </div>
+
+      {/* Buttons */}
+      <button
+        id="data-carousel-prev"
+        type="button"
+        onClick={prevIndexHandler}
+        className="absolute top-0 left-0 z-30 flex justify-center items-center h-full px-4 ml-16 cursor-pointer group focus:outline-none"
+      >
+        <span className="inline-flex justify-center items-center w-16 h-16 ml-16 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            className="w-4 h-4 text-black dark:text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 1 1 5l4 4"
+            />
+          </svg>
+        </span>
+      </button>
+
+      <button
+        id="data-carousel-next"
+        type="button"
+        onClick={nextIndexHandler}
+        className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 mr-16 cursor-pointer group focus:outline-none"
+      >
+        <span className="inline-flex items-center justify-center w-16 h-16 mr-16 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            className="w-4 h-4 text-black dark:text-gray-800"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 9 4-4-4-4"
+            />
+          </svg>
+        </span>
+      </button>
     </div>
   );
 }
