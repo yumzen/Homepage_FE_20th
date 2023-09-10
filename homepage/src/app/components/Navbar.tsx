03a20c38ca253243e7923731a73635b7f2a20698
yumzen <yeom.ji.eun35@gmail.com>
2023-09-10 20:21:54 +0900

    Design: Navbar design 수정

--- homepage/src/app/components/Navbar.tsx
+++ homepage/src/app/components/Navbar.tsx
@@ -41,42 +41,40 @@ let Links =[
 return (
     <nav className="w-[full] h-[104px] flex relative bg-[white] z-50 font-['pretendard']">
         <NavBg>
-        <div className="lg:flex">
+        <div className="lg:flex ">
             <div className="flex justify-between">
                 <Link href="/" key="home">
-                    <Image src="/assets/images/layout/Logo.svg" alt="Logo" width={165} height={30} onClick={showMenu?toggleMenu:undefined} className= "ml-[20px] lg:ml-[12.5vw] w-[165px] h-[30px] max-w-[165px] max-h-[30px]" priority/>
+                    <Image src="/assets/images/layout/Logo.svg" alt="Logo" width={165} height={30} onClick={showMenu?toggleMenu:undefined} className= "ml-[20px] lg:ml-[12.5vw] w-[165px] lg:w-[12.5vw] h-[30px] lg:max-w-[10vw]  max-w-[165px] max-h-[30px]" priority/>
                 </Link>
                 <div className="lg:hidden">
                     <button className="px-2 outline-none" onClick={toggleMenu}>
-                    {showMenu ? (<Image src="assets/images/layout/close.svg" width={32} height={32} alt="logo" /> ) : ( <Image src="assets/images/layout/hamburger.svg" width={32} height={32} alt="logo" className="focus:border-none active:border-none" />)}
+                    {showMenu ? (<Image src="/assets/images/layout/close.svg" width={32} height={32} alt="close" /> ) : ( <Image src="/assets/images/layout/hamburger.svg" width={32} height={32} alt="hamburger" className="focus:border-none active:border-none" />)}
                     </button>
                 </div>
             </div>
-            <div className={`${ showMenu ? "flex flex-col absolute mt-[20px] bg-[white] " : "hidden flex-row lg:pl-[2.6vw]" } lg:flex w-full items-center`}>
-                <ul className={`${ showMenu ? "flex-col py-[40px] mt-12 mb-4 ": "lg:gap-[7vw] flex-col lg:flex lg:flex-row"}`}>
+            <div className={`${ showMenu ? "flex flex-col absolute mt-[20px] bg-[white] " : "hidden flex-row ml-0 lg:mr-[12.5vw]" } lg:flex w-full items-center`}>
+                <ul className={`${ showMenu ? "flex-col py-[40px] mt-12 mb-4 ": " lg:w-[50vw] flex-col lg:flex lg:flex-row"}`}>
                     { Links.map((link) => (
                         <Link key={link.name} href={link.link} onClick={() => setFocusedLink(link.name)}>
-                            <div className={`${ focusedLink === link.name || pathname?.startsWith(link.link)
-                                    ? showMenu
+                            <div className={`${ showMenu
                                         ? "font-[700]"
-                                        : "= border-t-4 flex items-center justify-center border-t-[#281CFF]"
-                                    : ""
+                                        : "flex items-center justify-center w-[12.5vw]"
                                 }`}>
-                                <li key={link.name} onClick={showMenu ? toggleMenu : undefined} className={` text-[#000] text-center ${ showMenu ? "text-[20px] w-[100%] h-[100%] mb-4":"text-[16px] w-[80px] h-[32px] font-[600] leading-[19px] flex-shrink-0 flex justify-center items-center"}`}>
+                                <li key={link.name} onClick={showMenu ? toggleMenu : undefined} className={` text-[#000] ${ focusedLink === link.name || pathname?.startsWith(link.link) ? "border-t-4 border-t-[#281CFF]" : ""}  ${ showMenu ? "text-[20px] w-[100%] h-[100%] mb-4 text-center":"text-[16px] w-[80px]  h-[32px] font-[600] leading-[19px] flex-shrink-0 flex items-center justify-center"}`}>
                                 {link.name}
                                 </li>
                             </div>
                         </Link>
                         ))}
                 </ul>
-                <ul className={`${ showMenu ? " my-12 flex flex-row items-center justify-center mx-12":"hidden lg:flex flex-row ml-[5vw]"}`}>
-                    <div className={`${ showMenu ? "flex flex-row gap-[20px]" :"w-[12.5vw] flex items-center justify-between"}`}>
-                    <li className={`${ showMenu ? "w-[100%] h-[100%]" : "w-[100%] h-[100%] flex items-center justify-center"}`}>
+                <ul className={`${ showMenu ? " my-12 flex flex-row mx-12 items-center justify-center":"hidden lg:flex flex-row lg:w-[12.5vw] items-center justify-center lg:ml-[1.5vw]"}`}>
+                    <div className={`${ showMenu ? "flex flex-row gap-[20px]" :"w-[12.5vw] flex items-center mx-auto"}`}>
+                    <li className={`${ showMenu ? "w-[100%] h-[100%]" : "w-[4vw] h-[100%] flex items-center justify-center"}`}>
                     <Link href="http://pf.kakao.com/_UaIZG" target='_blank' passHref>
                         <Image src="/assets/images/layout/kakaotalk.svg" alt="카카오톡 채널" width={100} height={100} className="w-[28px] h-[28px]"/>
                     </Link>
                     </li>
-                    <li className={`${ showMenu ? "w-[100%] h-[100%] " : "w-[100%] h-[100%] flex items-center justify-center"}`}>
+                    <li className={`${ showMenu ? "w-[100%] h-[100%] " : "w-[4vw] h-[100%] flex items-center justify-center"}`}>
                     <Link
                         href="https://instagram.com/kahlua_band_?igshid=MzRlODBiNWFlZA=="
                         target='_blank'
@@ -85,7 +83,7 @@ return (
                         <Image src="/assets/images/layout/instagram.svg" alt="인스타그램" width={100} height={100} className="w-[28px] h-[28px]" />
                     </Link>
                     </li>
-                    <li className={`${ showMenu ? "w-[100%] h-[100%] " : "w-[100%] h-[100%] flex items-center justify-center"}`}>
+                    <li className={`${ showMenu ? "w-[100%] h-[100%] " : "w-[4vw] h-[100%] flex items-center justify-center"}`}>
                     <Link href="https://www.youtube.com/@kahluaband8409" target='_blank' passHref>
                         <Image src="/assets/images/layout/youtube.svg" alt="유튜브" width={100} height={100} className="w-[28px] h-[28px]" />
                     </Link>
