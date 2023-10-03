1c9f4550b4df052443a46bf730292110aac674c7
limgahyun <limgh2002@gmail.com>
2023-10-03 21:25:55 +0900

    Feat: mmain page design

--- homepage/src/pages/index.tsx
+++ homepage/src/pages/index.tsx
@@ -5,19 +5,20 @@ import rect1 from "/public/assets/images/Main/rectangle_1.svg"
 import rect3 from "/public/assets/images/Main/rectangle_3.svg"
 import rect4 from "/public/assets/images/Main/rectangle_4.svg"
 import circle from "/public/assets/images/Main/circle.svg"
-import cd from "/public/assets/images/Main/image_cd_full.svg"
+import cd from "/public/assets/images/Main/image_cd1.svg"
 
 
 
 export default function Home() {
 return (
-    <div className="h-screen">
-    <Background>
-        <Deco/>
-        <Headline/>
-        {/* animate-spin origin-bottom */}
-        <Image src={cd} alt=".." width={1500} className="absolute -mt-28  w-full h-[740px] z-20 object-cover object-top "/>
-    </Background>
+    //배경이 있으려면 높이를 무조건 설정해줘야되는데 nav bar가 높이에 포함이 안돼서 일단 높이를 screen-104px로 해둠
+    <div className="h-[calc(100vh-104px)]">
+        <Background>
+            <Deco/>
+            <Headline/>
+            {/* animate-spin origin-bottom */}
+            <Image src={cd} alt=".." width={1000} className="absolute bottom-0 right-0 h-[450px] z-20 object-cover object-bottom "/>
+        </Background>
     </div>
 );
 }
@@ -25,7 +26,7 @@ return (
 
 function Headline(){
     return(
-        <div className="ml-[calc(25%)] mt-10">
+        <div className="ml-[calc(22%)] mt-10 bg-transparent">
             <Image src={divider} alt="kahlua" width={108}/> 
             <p className="font-GothamBold text-7xl font-bold mt-2">Band Club</p>
             <p className="font-Salvar text-7xl font-bold mt-2">KAHLUA</p>
@@ -38,12 +39,12 @@ function Headline(){
 
 function Deco(){
     return(
-        <div className="">
+        <div className=" bg-transparent ">
             <Image src={rect3} alt=".." width={165} className="absolute right-0 top-0 w-[calc(12.5%)] mt-10"/>
             <Image src={circle} alt=".." width={160} className="absolute right-[calc(24%)] w-[calc(12%)] top-40"/>
             <Image src={circle} alt=".." width={160} className="absolute left-[calc(5%)] w-[calc(12%)] top-64"/>
             <Image src={rect4} alt=".." width={165} className="absolute left-0 top-[390px] w-[calc(12.5%)]"/>
-            <Image src={rect1} alt=".." width={165} className="absolute left-[calc(12.5%)] top-[460px] w-[calc(12.5%)]"/>
+            <Image src={rect1} alt=".." width={165} className="absolute left-[calc(12.5%)] bottom-0 w-[calc(12.5%)]"/>
         </div>
     )
 }
