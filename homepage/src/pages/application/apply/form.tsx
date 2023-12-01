6d54e386905b52bdeb7d923a14306c9e7483e23a
limgahyun <limgh2002@gmail.com>
2023-12-01 14:27:03 +0900

    Fix: application API

--- homepage/src/pages/application/apply/form.tsx
+++ homepage/src/pages/application/apply/form.tsx
@@ -1,39 +1,54 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const OPTIONS_Gender = [
-    { value: "man", name: "남성" },
-    { value: "woman", name: "여성" },
+    { value: "남성", name: "남성" },
+    { value: "여성", name: "여성" },
     ];
 
 const OPTIONS_Session1 = [
-    { value: "vocal", name: "보컬" },
-    { value: "drum", name: "드럼" },
-    { value: "guitar", name: "기타" },
-    { value: "bass", name: "베이스" },
-    { value: "synthesizer", name: "신디" },
+    { value: "보컬", name: "보컬" },
+    { value: "드럼", name: "드럼" },
+    { value: "기타", name: "기타" },
+    { value: "베이스", name: "베이스" },
+    { value: "신디", name: "신디" },
     ];
 
 const OPTIONS_Session2 = [
-    { value: "vocal", name: "보컬" },
-    { value: "drum", name: "드럼" },
-    { value: "guitar", name: "기타" },
-    { value: "bass", name: "베이스" },
-    { value: "synthesizer", name: "신디" },
+    { value: "보컬", name: "보컬" },
+    { value: "드럼", name: "드럼" },
+    { value: "기타", name: "기타" },
+    { value: "베이스", name: "베이스" },
+    { value: "신디", name: "신디" },
     ];
     
 
 export default function Form(){
     const [name, setName] = useState('');
-    const [phone_num, setPhonNum] = useState('');
+    const [phone_num, setPhoneNum] = useState('');
     const [birthdate, setBirthdate] = useState('');
-    const [gender, setGender] = useState('');
+    const [gender, setGender] = useState('남성');
     const [address, setAddress] = useState('');
-    const [first_preference, setFirst] = useState('');
-    const [second_preference, setSecond] = useState('');
+    const [first_preference, setFirst] = useState('보컬');
+    const [second_preference, setSecond] = useState('보컬');
     const [play_instrument, setInstrument] = useState('');
     const [motive, setMotive] = useState('');
-    const [selectedValue, setSelectedValue] = useState('');
+    const [isFormComplete, setIsFormComplete] = useState(false);
+
+
+    useEffect(() => {
+        const isDataComplete =
+        name.trim() !== '' &&
+        phone_num.trim() !== '' &&
+        birthdate.trim() !== '' &&
+        gender.trim() !== '' &&
+        address.trim() !== '' &&
+        first_preference.trim() !== '' &&
+        second_preference.trim() !== '' &&
+        true;
+        setIsFormComplete(isDataComplete);
+    }, [name, phone_num, birthdate, gender, address, first_preference, second_preference, play_instrument, motive]);
+    
 
     const handleSubmit = async () => {
         try {
@@ -48,10 +63,32 @@ export default function Form(){
                 play_instrument,
                 motive
             };
+
+            const validGenderChoices = ['남성', '여성'];
+            const validSessionChoices = ['보컬', '드럼', '기타', '베이스', '신디'];
+
+            // Validate gender
+            if (!validGenderChoices.includes(gender)) {
+                console.error(`${gender} is not a valid choice for gender.`);
+                return;
+            }
+
+            // Validate first_preference
+            if (!validSessionChoices.includes(first_preference)) {
+                console.error(`${first_preference} is not a valid choice for first preference.`);
+                return;
+            }
+
+            // Validate second_preference
+            if (!validSessionChoices.includes(second_preference)) {
+                console.error(`${second_preference} is not a valid choice for second preference.`);
+                return;
+            }
+
             const response = await axios.post('http://127.0.0.1:8000/application/apply/', formData);
             console.log(response)
 
-            if (response.status === 200) {
+            if (response.status === 200 || response.status === 201) {
                 console.log('요청이 성공적으로 처리되었습니다.');
                 console.log('응답 데이터:', response.data);
             } else {
@@ -62,31 +99,44 @@ export default function Form(){
             console.error('Error submitting data:', error);
         }
     };
+
+    type SelectBoxProps = {
+        options : {value: string; name : string}[];
+        onChange : (value: string) => void;
+        value : string;
+    }
     
-    const SelectBox = (props: any) => {
-    
-        const handleOptionClick = (value:any) => {
-            setSelectedValue(value);
+    const SelectBox: React.FC<SelectBoxProps> = (props) => {
+        const handleOptionChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+            props.onChange(e.target.value);
         };
     
         return (
-        <select className=" text-base cursor-pointer w-full h-[64px] rounded-[10px] border border-[#464646] border-solid text-center">
-            <option value="" disabled >선택</option>
-            {props.options.map((option: any) => (
-            <option key={option.value} value={option.value} onClick={handleOptionClick}>
-                {option.name}
-            </option>
-            ))}
-        </select>
-        );
+            <div className="flex flex-col">
+              <select
+                className="text-base cursor-pointer w-full h-[64px] rounded-[10px] border border-[#464646] border-solid text-center"
+                onChange={handleOptionChange}
+                value={props.value}
+              >
+                <option value="" disabled>
+                  선택
+                </option>
+                {props.options.map((option: any) => (
+                  <option key={option.value} value={option.value}>
+                    {option.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          );
     };
 
     function Name(){
         return(
             <div className="w-[calc(50%-16px)] h-auto">
-                <p className="text-xl font-bold">이름</p>
+                <div className="text-xl font-bold">이름</div>
                 <div className="input-with-placeholder relative h-[64px] flex-shrink-0 border border-[#464646] border-solid rounded-[10px] mt-4">
-                    <input value={name} type="text" className="text-base w-full h-full rounded-[10px] p-4 " placeholder="지원자 이름" onChange={(e) => setName(e.target.value)}/>
+                    <input value={name} minLength={30} type="text" className="text-base w-full h-full rounded-[10px] p-4 " placeholder="지원자 이름" onChange={(e) => setName(e.target.value)}/>
                 </div>
             </div>
         )
@@ -108,7 +158,7 @@ export default function Form(){
             <div className="w-[calc(50%-16px)] h-auto">
                 <p className="text-xl font-bold">전화번호</p>
                 <div className="input-with-placeholder relative w-full h-[64px] flex-shrink-0 border border-[#464646] border-solid rounded-[10px] mt-4">
-                    <input value={phone_num} type="text" className="text-base w-full h-full rounded-[10px] p-4" placeholder="기호없이 11자리로 입력해주세요 '예:01012345678'" onChange={(e) => setPhonNum(e.target.value)}/>
+                    <input value={phone_num} type="text" className="text-base w-full h-full rounded-[10px] p-4" placeholder="기호없이 11자리로 입력해주세요 '예:01012345678'" onChange={(e) => setPhoneNum(e.target.value)}/>
                 </div>
             </div>
         )
@@ -119,7 +169,7 @@ export default function Form(){
             <div className="w-1/4 h-auto">
                 <p className="text-xl font-bold">성별</p>
                 <div className="h-[64px] flex-shrink-0 rounded-[10px] mt-4">
-                    <SelectBox options={OPTIONS_Gender} onChange={() => setGender(selectedValue)}/>
+                    <SelectBox options={OPTIONS_Gender} onChange={(value) => setGender(value)} value={gender}/>
                 </div>
             </div>
         )
@@ -142,7 +192,7 @@ export default function Form(){
                 <span className="text-xl font-bold">1지망 선택 (필수)</span>
                 <span className="text-xs text-[#8E8E8E] ml-2">세션 중 1가지 선택</span>
                 <div className="input-with-placeholder relative w-full h-[64px] flex-shrink-0 rounded-[10px] mt-4">
-                    <SelectBox options={OPTIONS_Session1} onClick={setFirst(selectedValue)}/>
+                    <SelectBox options={OPTIONS_Session1} onChange={(value) => setFirst(value)} value={first_preference}/>
                 </div>
             </div>
         )
@@ -154,7 +204,7 @@ export default function Form(){
                 <span className="text-xl font-bold">2지망 선택 (필수)</span>
                 <span className="text-xs text-[#8E8E8E] ml-2">1지망에 선택한 세션을 제외하고 선택</span>
                 <div className="input-with-placeholder relative w-full h-[64px] flex-shrink-0 mt-4">
-                    <SelectBox options={OPTIONS_Session2} onClick={setSecond(selectedValue)}/>
+                    <SelectBox options={OPTIONS_Session2} onChange={(value) => setSecond(value)} value={second_preference}/>
                 </div>
             </div>
         )
@@ -209,7 +259,7 @@ export default function Form(){
             <Motive/>
 
             <div className="flex flex-col justify-center items-center">
-                <button className="w-80 h-16 bg-ocean rounded-[10px] mt-28 mb-32 text-[#FFFFFF]" onClick={handleSubmit}>
+                <button disabled={!isFormComplete} className="w-80 h-16 bg-ocean rounded-[10px] mt-28 mb-32 text-[#FFFFFF]" onClick={handleSubmit}>
                     제출하기
                 </button>
             </div>
