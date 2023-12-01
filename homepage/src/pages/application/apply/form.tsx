7cdce7b016df6546660e21842df7c7dd7801f624
limgahyun <limgh2002@gmail.com>
2023-12-01 14:46:15 +0900

    Fix: input error

--- homepage/src/pages/application/apply/form.tsx
+++ homepage/src/pages/application/apply/form.tsx
@@ -234,29 +234,74 @@ export default function Form(){
         <>
             <div className="flex flex-col justify-center pt-8 pb-10">
                 <div className="inline-flex flex-wrap flex-row space-x-8 items-start">
-                    <Name/>
-                    <Birth/>
+                    <div className="w-[calc(50%-16px)] h-auto">
+                        <p className="text-xl font-bold">이름</p>
+                        <div className="input-with-placeholder relative h-[64px] flex-shrink-0 border border-[#464646] border-solid rounded-[10px] mt-4">
+                            <input value={name} minLength={30} type="text" className="text-base w-full h-full rounded-[10px] p-4 " placeholder="지원자 이름" onChange={(e) => setName(e.target.value)}/>
+                        </div>
+                    </div>
+                    <div className="w-[calc(50%-16px)] h-auto">
+                        <p className="text-xl font-bold">생년월일</p>
+                        <div className="input-with-placeholder relative h-[64px] flex-shrink-0 border border-[#464646] border-solid rounded-[10px] mt-4">
+                            <input value={birthdate} type="text" className="text-base w-full h-full rounded-[10px] p-4" placeholder="8자리로 입력해주세요" onChange={(e) => setBirthdate(e.target.value)}/>
+                        </div>
+                    </div>
                 </div>
                 <div className="inline-flex flex-wrap flex-row space-x-8 items-start pt-8">
-                    <Number/>
-                    <Gender/>
+                    <div className="w-[calc(50%-16px)] h-auto">
+                        <p className="text-xl font-bold">전화번호</p>
+                        <div className="input-with-placeholder relative w-full h-[64px] flex-shrink-0 border border-[#464646] border-solid rounded-[10px] mt-4">
+                            <input value={phone_num} type="text" className="text-base w-full h-full rounded-[10px] p-4" placeholder="기호없이 11자리로 입력해주세요 '예:01012345678'" onChange={(e) => setPhoneNum(e.target.value)}/>
+                        </div>
+                    </div>
+                    <div className="w-1/4 h-auto">
+                        <p className="text-xl font-bold">성별</p>
+                        <div className="h-[64px] flex-shrink-0 rounded-[10px] mt-4">
+                            <SelectBox options={OPTIONS_Gender} onChange={(value) => setGender(value)} value={gender}/>
+                        </div>
+                    </div>
                 </div>
-                <Address/>
+                <div className="mt-8">
+                    <p className="text-xl font-bold">거주지</p>
+                    <div className="input-with-placeholder relative w-full h-[64px] flex-shrink-0 border border-[#464646] border-solid rounded-[10px] mt-4">
+                        <input value={address} type="text" className="text-base w-full h-full rounded-[10px] p-4" placeholder="기숙사의 경우 '예:2기숙사/부산'으로 입력해주세요" onChange={(e) => setAddress(e.target.value)}/>
+                    </div>
+                </div>
             </div>
 
             <div className="mt-2 h-[3px] mx-auto bg-[#D3D3D3]"/>
 
             <div className="flex flex-col justify-center pt-8 pb-10">
                 <div className="inline-flex flex-wrap flex-row space-x-8 items-start">
-                    <Session1/>
-                    <Session2/>
+                    <div className="w-[calc(50%-16px)] h-auto">
+                        <span className="text-xl font-bold">1지망 선택 (필수)</span>
+                        <span className="text-xs text-[#8E8E8E] ml-2">세션 중 1가지 선택</span>
+                        <div className="input-with-placeholder relative w-full h-[64px] flex-shrink-0 rounded-[10px] mt-4">
+                            <SelectBox options={OPTIONS_Session1} onChange={(value) => setFirst(value)} value={first_preference}/>
+                        </div>
+                    </div>
+                    <div className="w-[calc(50%-16px)] h-auto">
+                        <span className="text-xl font-bold">2지망 선택 (필수)</span>
+                        <span className="text-xs text-[#8E8E8E] ml-2">1지망에 선택한 세션을 제외하고 선택</span>
+                        <div className="input-with-placeholder relative w-full h-[64px] flex-shrink-0 mt-4">
+                            <SelectBox options={OPTIONS_Session2} onChange={(value) => setSecond(value)} value={second_preference}/>
+                        </div>
+                    </div>
                 </div>
-                <Experience/>
+                <div className="mt-8">
+                    <p className="text-xl font-bold">다룰 줄 아는 악기 (선택)</p>
+                    <div className="input-with-placeholder relative w-full h-[64px] flex-shrink-0 border border-[#464646] border-solid rounded-[10px] mt-4">
+                        <input value={play_instrument} type="text" className="text-base  w-full h-full rounded-[10px] p-4" placeholder="" onChange={(e) => setInstrument(e.target.value)}/>
+                    </div>
+                </div>
             </div>
 
             <div className="mt-2 h-[3px] mx-auto bg-[#D3D3D3]"/>
             
-            <Motive/>
+            <div className="pt-8">
+                <p className="text-xl font-bold">지원 동기 (자유롭게 작성)</p>
+                <textarea value={motive} className="text-base w-full h-[256px] flex-shrink-0 border border-[#464646] border-solid rounded-[10px] mt-4 p-4" placeholder="" onChange={(e) => setMotive(e.target.value)}/>
+            </div>
 
             <div className="flex flex-col justify-center items-center">
                 <button disabled={!isFormComplete} className="w-80 h-16 bg-ocean rounded-[10px] mt-28 mb-32 text-[#FFFFFF]" onClick={handleSubmit}>
