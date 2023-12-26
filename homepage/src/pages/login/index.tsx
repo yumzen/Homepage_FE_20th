f3fc0ba31081bac7b8f358b534c6b7cbb50eb762
caminobelllo <alstpgml0812@gmail.com>
2023-12-26 14:42:44 +0900

    tailwind 컬러 팔레트 추가

--- homepage/src/pages/login/index.tsx
+++ homepage/src/pages/login/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="w-full h-full">
+      <div className="w-[780px] h-[560px] bg-black">ss</div>
+    </div>
+  );
 };
 
 export default Login;
