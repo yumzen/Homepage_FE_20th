6e13c8b50cf9244998b659aaf1aa4a97bf3ca045
yumzen <yeom.ji.eun35@gmail.com>
2023-12-31 05:20:26 +0900

    Design: 반응형 에러 모달창

--- homepage/src/pages/tickets/freshman_ticket/error_modal.tsx
+++ homepage/src/pages/tickets/freshman_ticket/error_modal.tsx
@@ -32,16 +32,18 @@ const error_modal = () => {
     }, []);
     return !isClose ? (
         <div onClick={handleOverlayClick} className= "fixed z-50 top-0 left-0 right-0 bottom-0 bg-[#0000008a] flex justify-center items-center">
-            <div className="font-['pretendard'] w-[580px] h-[290px] bg-[#FFF] flex-shrink-0 fixed z-20">
-                <button onClick={handleIsClose} className="ml-[544px] w-[36px] h-[38px] flex-col items-center flex justify-center text-[20px] font-[700]">x</button>
+            <div className="font-['pretendard'] w-[300px] h-[200px] sm:w-[580px] sm:h-[290px] bg-[#FFF] flex-shrink-0 fixed rounded-[3px] z-20">
+                <button onClick={handleIsClose} className="ml-[280px] h-[22px] sm:h-[30px] sm:ml-[550px] flex-col items-center flex justify-center">
+                <Image src="/assets/images/layout/close.svg" width={36} height={38} alt="close" className="w-[18px] h-[20px] sm:w-[25px] sm:h-[25px]"/>
+                </button>
                 <div className="w-[100%] h-[1px] bg-[#D3D3D3]"/>
-                <div className="flex flex-col items-center text-center content-center mt-[40px] leading-normal">
-                    <Image src="/assets/images/tickets/divider_medium.svg" alt="ticket" width={52} height={12}/>
-                    <p className="font-[700] mt-[12px] text-[24px] leading-[28px]">이미 예약한 정보입니다.</p>
-                    <p className="mt-[20px] font-[500] text-[14px] laeding-[21px] text-[#4A4A4A]">입력하신 정보를 다시 한번 확인해주세요.</p>
-                    <button onClick={() => window.location.reload()}  className="mt-[20px] felx items-center w-[100px] h-[24px] justify-center rounded-[10px] bg-[#281CFF] text-[white]  text-[12px] font-[700] leading-[17px] text-center">다시 입력하기</button>
+                <div className="flex flex-col items-center text-center content-center mt-[12px] sm:mt-[40px] leading-normal">
+                    <Image src="/assets/images/tickets/divider_medium.svg" alt="ticket" width={52} height={12} className="sm:w-[52px] sm:h-[12px] w-[30px] h-[10px]"/>
+                    <p className="font-[700] mt-[12px] text-[16px] sm:text-[24px] leading-[28px]">이미 예약한 정보입니다.</p>
+                    <p className="mt-[8px] sm:mt-[20px] sm:font-[500] text-[10px] sm:text-[14px] leading-[21px] text-[#4A4A4A]">입력하신 정보를 다시 한번 확인해주세요.</p>
+                    <button onClick={() => window.location.reload()}  className="mt-[8px] sm:mt-[20px] felx items-center w-[70px] h-[20px] sm:w-[100px] sm:h-[24px] justify-center rounded-[3px] bg-[#281CFF] text-[white] text-[10px] sm:text-[12px] font-[700] leading-[17px] text-center">다시 입력하기</button>
                     <Link href="freshman_ticket/delete">
-                    <button className="mt-[20px] felx items-center w-[120px] h-[24px] justify-center rounded-[10px] bg-[#939393] text-[white]  text-[12px] font-[700] leading-[17px] text-center">예매내역 확인하기</button>
+                    <button className="mt-[12px] sm:mt-[20px] felx items-center  w-[85px] h-[20px] sm:w-[120px] sm:h-[24px] justify-center rounded-[3px] bg-[#939393] text-[white] text-[10px] sm:text-[12px] font-[700] leading-[17px] text-center">예매내역 확인하기</button>
                     </Link>
                 </div>
                 </div>
