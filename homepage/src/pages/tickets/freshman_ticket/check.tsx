667bec569f883c4740b97857b8cdb5efd891a1bf
yumzen <yeom.ji.eun35@gmail.com>
2024-01-03 11:51:10 +0900

    Design: 신입생 티켓 취소 반응형

--- homepage/src/pages/tickets/freshman_ticket/check.tsx
+++ homepage/src/pages/tickets/freshman_ticket/check.tsx
@@ -71,37 +71,37 @@ const freshman_delete = () => {
 
     
     return  (
-        <div className = "h-[650px]">
+        <div className = "h-[700px]">
         <Background>
-            <div className="font-['pretendard'] mx-[12.5vw] flex items-center flex-col mb-[84px]">
-                <div className="flex flex-col items-center text-center content-center mt-[40px] leading-normal">
-                    <Image src="/assets/images/tickets/divider_medium.svg" alt="ticket" width={52} height={12}/>
-                    <p className="font-[700] mt-[12px] text-[24px] leading-[28px]">예매내역 취소</p>
-                    <p className="mt-[20px] font-[500] text-[14px] laeding-[21px] text-[#4A4A4A]">[예매 취소하기] 버튼을 누르시면 예매 취소가 완료됩니다.</p>
+        <div className="font-['pretendard'] mx-[12.5vw] flex items-center flex-col mb-[84px] ">
+                <div className="flex flex-col items-center mx-[12.5vw] text-center content-center mt-[40px] leading-normal">
+                    <Image src="/assets/images/tickets/divider_medium.svg" alt="ticket" width={52} height={12}  className="w-[50px] h-[11px] sm:w-[75px] sm:h-[17px]"/>
+                    <p className="mt-[8px] sm:mt-[16px] font-[700] text-[20px] sm:text-[32px] leading-[42px] whitespace-nowrap flex flex-row">예매내역 취소</p>
+                    <p className="mt-[16px] sm:mt-[32px] font-[500] text-[10px] sm:text-[14px] leading-[21px] text-[#4A4A4A] whitespace-nowrap">[예매 취소하기] 버튼을 누르시면 예매 취소가 완료됩니다.</p>
                 </div>
                 {reservation_id && (
-                    <div className="mt-[48px] mx-auto items-center content-center flex flex-col ">
-                        <div className="mt-[16px] mx-auto bg-[#F1F5FF] w-[516px] h-[120px] flex-shrink-0 rounded-[8px]">
-                            <div className="flex flex-row align-center justify-center">
-                                <div className="mt-[15px] ml-[24px] w-[118px] h-[19px] flex text-center justify-center  items-center text-[12px] font-[700]"> 예매번호 </div>
-                                <div className="mt-[15px] mx-auto  w-[118px] h-[19px]  flex text-center justify-center  items-center text-[12px] font-[700]"> 예매자 성명 </div>
-                                <div className="mt-[15px] mr-[24px] w-[118px] h-[19px]  flex text-center justify-center  items-center text-[12px] font-[700]"> 취소 가능 여부 </div>
+                    <div className="mt-[12px] sm:mt-[48px] mx-auto items-center content-center flex flex-col ">
+                        <div className="mt-[10px] sm:mt-[32px] mx-auto bg-[#F1F5FF] w-[75vw] sm:w-[400px] md:w-[516px] h-[120px] flex-shrink-0 rounded-[8px]">
+                            <div className="flex flex-row align-center justify-center text-[10px] sm:text-[12px] font-[700]">
+                                <div className="mt-[15px] ml-[24px] sm:w-[80px] md:w-[118px] h-[19px] flex text-center justify-center  items-center"> 예매번호 </div>
+                                <div className="mt-[15px] mx-auto sm:w-[80px] md:w-[118px] h-[19px]  flex text-center justify-center  items-center"> 예매자 성명 </div>
+                                <div className="mt-[15px] mr-[24px] sm:w-[80px] md:w-[118px] h-[19px]  flex text-center justify-center  items-center "> 취소 가능 여부 </div>
                             </div>
                             <div className="mt-[21px] h-[1px] w-[100%] bg-[#D3D3D3] "/>
-                                <div className="flex flex-row align-center justify-center">
-                                    <div className="mt-[19px] ml-[24px] w-[118px]  h-[21px] flex text-center justify-center items-center text-[14px] font-[500]"> {reservation_id} </div>
-                                    <div className="mt-[19px] mx-auto w-[118px] flex text-center justify-center  items-center text-[14px] font-[500]"> {buyer} </div>
-                                    <div className="mt-[19px] mr-[24px] w-[118px] flex text-center justify-center  items-center text-[14px] font-[500]"> {cancelable ? "취소 가능" : "취소 불가능"} </div>
+                                <div className="flex flex-row align-center justify-center text-[10px] sm:text-[14px] font-[500]">
+                                    <div className="mt-[19px] ml-[24px] w-[118px]  h-[21px] flex text-center justify-center items-center "> {reservation_id} </div>
+                                    <div className="mt-[19px] mx-auto w-[118px] flex text-center justify-center  items-center "> {buyer} </div>
+                                    <div className="mt-[19px] mr-[24px] w-[118px] flex text-center justify-center  items-center "> {cancelable ? "취소 가능" : "취소 불가능"} </div>
                                 </div>
-                        </div>
-                        <div className="flex mt-[16px] ml-[4px] flex-col w-[516px]">
-                            <p className="ml-0 text-left text-[#4A4A4A] text-[12px] leading-[21px]"> 예매 취소 인증 절차입니다.</p>
-                            <p className="ml-0 text-left text-[#4A4A4A] text-[12px] leading-[21px]"> 정말 취소하시려면 예매하실 때 입력한 학번을 입력해주세요.</p>
-                            <input type="text" value={input_sid} onChange={(e) => set_sid(e.target.value)} placeholder="학번을 입력해 주세요." className="mt-[10px] bg-[#FFF] border  border-[#4A4A4A] rounded-lg text-[14px] outline-[#000] w-[180px] h-[36px] text-[black] px-[8px]" onKeyDown={handleInputKeyPress}/>
+                            </div>
+                        <div className="flex mt-[16px] mx-auto sm:ml-[4px] flex-col w-[75vw] sm:w-[300px] md:w-[516px]">
+                            <p className="ml-0 text-left text-[#4A4A4A] text-[10px] sm:text-[12px] leading-[21px]"> 예매 취소 인증 절차입니다.</p>
+                            <p className="ml-0 text-left text-[#4A4A4A] text-[10px] sm:text-[12px] leading-[21px]"> 정말 취소하시려면 예매하실 때 입력한 학번을 입력해주세요.</p>
+                            <input type="text" value={input_sid} onChange={(e) => set_sid(e.target.value)} placeholder="학번을 입력해 주세요." className="mt-[10px] bg-[#FFF] border  border-[#4A4A4A] rounded-[3px] text-[10px] sm:text-[14px] outline-[#000] w-[150px] h-[28px] sm:w-[180px] sm:h-[36px] text-[black] px-[8px]" onKeyDown={handleInputKeyPress}/>
                             {!validSid && <p className="text-[#F00] text-[10px] font-[400] leading-[19px] align-center mt-[2px]">⚠️ 잘못된 입력 정보입니다.</p>}
                         </div>
-                        <div className="w-[514px] h-[48px] mt-[40px] mx-auto flex items-center">
-                        <button className="w-full h-full bg-[#E8E8E8] rounded-[8px] text-center text-[#000] hover:text-[#FFF] text-[14px] font-[600] leading-[19px] hover:bg-[#281CFF]" onClick={handleCancelReservation}>
+                        <div className="w-[75vw] sm:w-[400px] md:w-[514px] h-[36px] sm:h-[48px] mt-[20px] sm:mt-[48px] mx-auto flex items-center">
+                        <button onClick={handleCancelReservation} className="w-full h-full bg-[#E8E8E8] rounded-[8px] text-center text-[#000] hover:text-[#FFF] text-[14px] font-[600] leading-[19px] hover:bg-[#281CFF]">
                             예매 취소하기
                         </button>
                         </div>
