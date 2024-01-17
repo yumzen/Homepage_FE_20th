a103b566012e566033dac9f19d96fec21edc373c
caminobelllo <alstpgml0812@gmail.com>
2024-01-17 19:46:34 +0900

    Feat: access 토큰 만료 시 에러 처리 구현

--- homepage/src/apis/admin.ts
+++ homepage/src/apis/admin.ts
@@ -1,32 +1,60 @@
 import axios from "axios";
+import { getNewRefreshToken } from "./refresh";
 
 // admin page에서 각 함수 호출해서 데이터 가져와야 함
 
 // 어드민 티켓 데이터 가져오기
 export const getTicketAdmin = async () => {
   const access = localStorage.getItem("access");
-  // api 연결 필요
-  const res = await axios.get("", {
-    headers: {
-      Authorization: access,
-    },
-  });
 
-  // response 타입 보고 수정 가능
-  return res.data;
+  try {
+    // api 연결 필요
+    const res = await axios.get("", {
+      headers: {
+        Authorization: access,
+      },
+    });
+
+    // response 타입 보고 수정 가능
+    return res.data;
+  } catch (error) {
+    // 테스트 필요
+    if (error instanceof Error) {
+      console.log(error.message);
+      // 401 일 때 (토큰 만료 시)
+      //   const { access, refresh } = await getNewRefreshToken();
+      //   error.config.headers.Authorization = access;
+      //   localStorage.setItem("access", access);
+      //   localStorage.setItem("refresh", refresh);
+      //   return axios.get(error.config.url, error.config).data;
+    }
+  }
 };
 
 // 어드민 지원 데이터 가져오기
 export const getApplyAdmin = async () => {
   const access = localStorage.getItem("access");
 
-  // api 연결 필요
-  const res = await axios.get("", {
-    headers: {
-      Authorization: access,
-    },
-  });
+  try {
+    // api 연결 필요
+    const res = await axios.get("", {
+      headers: {
+        Authorization: access,
+      },
+    });
 
-  // response 타입 보고 수정 가능
-  return res.data;
+    // response 타입 보고 수정 가능
+    return res.data;
+  } catch (error) {
+    // 테스트 필요
+    if (error instanceof Error) {
+      console.log(error.message);
+      // 401 일 때 (토큰 만료 시)
+      //   const { access, refresh } = await getNewRefreshToken();
+      //   error.config.headers.Authorization = access;
+      //   localStorage.setItem("access", access);
+      //   localStorage.setItem("refresh", refresh);
+      //   return axios.get(error.config.url, error.config).data;
+    }
+  }
 };
