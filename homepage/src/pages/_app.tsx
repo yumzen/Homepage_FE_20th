873b12c4ff063c68f1e332f576618ff0ac993f49
caminobelllo <alstpgml0812@gmail.com>
2024-01-17 18:50:57 +0900

    브라우저 탭 이름 변경

--- homepage/src/pages/_app.tsx
+++ homepage/src/pages/_app.tsx
@@ -1,21 +1,22 @@
-import { AppProps } from 'next/app';
-import '../app/globals.css'; 
-import RootLayout from '@/app/layout';
-import Head from 'next/head';
+import { AppProps } from "next/app";
+import "../app/globals.css";
+import RootLayout from "@/app/layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <>
-        <Head>
+  return (
+    <>
+      <Head>
+        <title>KAHLUA BAND</title>
         <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
         <script src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-        </Head>
-            <RootLayout>
-                <Component {...pageProps} />
-            </RootLayout>
-        </>
-    );
+      </Head>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </>
+  );
 }
 
 export default MyApp;
