9bd7ab78a64763d821e1e8607cbbab6c43624d0d
yumzen <yeom.ji.eun35@gmail.com>
2024-01-19 00:39:28 +0900

    Fixed: Compile Error 수정

--- homepage/src/pages/_app.tsx
+++ homepage/src/pages/_app.tsx
@@ -7,12 +7,8 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
-        <Head>
-        {/*<link rel="icon" href="/assets/images/layout/favicon.png" />
-        <title>Kahlua Band</title>*/}
         <Script src="https://code.jquery.com/jquery-1.12.4.min.js"></Script>
         <Script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></Script>
-        </Head>
             <RootLayout>
                 <Component {...pageProps} />
             </RootLayout>
