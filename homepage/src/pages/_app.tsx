656d93198858f9f75b013ccab7048f58a169b479
yumzen <yeom.ji.eun35@gmail.com>
2024-01-19 07:08:21 +0900

    Feat: 파비콘 적용

--- homepage/src/pages/_app.tsx
+++ homepage/src/pages/_app.tsx
@@ -8,6 +8,7 @@ return (
 <>
     <Head>
     <title>KAHLUA BAND</title>
+    <link rel="icon" href="/kahlua.png" />
     </Head>
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"></script>
