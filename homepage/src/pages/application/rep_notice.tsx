1d2de58c6693284ad265deb2c6515643ca314276
yumzen <yeom.ji.eun35@gmail.com>
2024-01-20 01:03:32 +0900

    Fixed: build error 수정

--- homepage/src/pages/application/rep_notice.tsx
+++ homepage/src/pages/application/rep_notice.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import band from "/public/assets/images/recruit/band.svg";
 import band1 from "/public/assets/images/recruit/band1.png";
 import recruit_rocket from "/public/assets/images/recruit/divider_medium.svg";
+import Link from "next/link";
 
 export default function RepNotice() {
     return (
@@ -20,9 +21,9 @@ export default function RepNotice() {
                     <br/>
                     면접날짜 : 2024.03.18(월)
                 </p>
-                <a className="flex justify-center items-center h-[64px] w-[260px] x:w-1/2 rounded-[300px] bg-ocean mt-14 text-xl x:text-2xl font-semibold text-[#FFFFFF]" href="/application/apply">
+                <Link className="flex justify-center items-center h-[64px] w-[260px] x:w-1/2 rounded-[300px] bg-ocean mt-14 text-xl x:text-2xl font-semibold text-[#FFFFFF]" href="/application/apply">
                     KAHLUA 23기 지원하기
-                </a>
+                </Link>
 
             </div>
         </div>
